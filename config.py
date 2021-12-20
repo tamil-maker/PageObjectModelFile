46cab65e146059805faf6e636bb4e3a356145e66
tamil-maker <91489089+tamil-maker@users.noreply.github.com>
2021-12-20 17:27:00 -0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+class TestData:
+
+    CHROME_EXECUTABLE_PATH = "user/Ramya/probable_carnival/chromedriver"
+    FIREFOX_EXECUTABLE_PATH = "user/Ramya/probable_carnival/geckodriver"
+    
+    BASE_URL="https://www.cansrdev.com/clinical/dashboard/"
+    USER_NAME="joercampell1@yopmail.com"
+    PASSWORD="Joe123!"   
+    LOGIN_PAGE_TITLE = "Cansr | Login"
